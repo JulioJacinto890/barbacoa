bb00cf46f08cf5cf7394e1119e06c1909d7e87fd
Darkx <darkfoxy879@gmail.com>
2024-10-24 14:46:39 -0600

    nav1

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,27 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      barbacoa343
+    <header className="flex justify-between items-center p-4 bg-white border-b">
+    {/* Logo */}
+    <div className="flex items-center">
+      <img src="fiverr-logo.png" alt="Fiverr logo" className="h-10" />
     </div>
+
+    {/* Menu */}
+    <nav>
+      <ul className="flex space-x-6">
+        <li><a href="#" className="text-gray-800">Fiverr Pro</a></li>
+        <li><a href="#" className="text-gray-800">Explora</a></li>
+        <li><a href="#" className="text-gray-800">Español</a></li>
+        <li><a href="#" className="text-gray-800">Conviértete en vendedor</a></li>
+        <li><a href="#" className="text-gray-800">Iniciar sesión</a></li>
+        <li>
+          <a href="#" className="px-4 py-2 border border-green-500 text-green-500 rounded hover:bg-green-500 hover:text-white">
+            Únete
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
   )
 }
