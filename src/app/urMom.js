d28dd5667456019927afb2164a4b0e86354f4aaf
RubenAcosta16 <1511ruben@gmail.com>
2025-02-05 13:44:11 -0600

    delete ur mom

--- src/app/urMom.js
+++
@@ -1,7 +0,0 @@
-let urMom = { name: "urMom", age: 69 }
-let urMom2 = { name: "urMom2", age: 69 }
-
-let elOmar = { name: "elOmar", age: 20 }
-let julio = { name: "julio", age: 20 }
-
-let personas= [urMom, urMom2, elOmar]
